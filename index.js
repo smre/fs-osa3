f650b96c0fbd49ac399d3e0c1a77eaac5e9511b7
sumire <me@sumire.io>
2018-02-09 14:51:58 +0200

    Set up Heroku configuration

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ app.get('/info', (req, res) => {
     `<p>Puhelinluettelossa ${persons.length} ihmisen tiedot</p><p>${new Date()}</p>`);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
